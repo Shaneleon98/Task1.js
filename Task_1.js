bc1158ac4bf0ef031a1f377b59bc1db902ee18bd
Shane Leon <shaneleon2000@gmail.com>
2024-09-23 20:25:35 +0530

    first commit

---
+++ Task_1.js
@@ -0,0 +1,64 @@
+//  Data Types in JavaScript
+let name = "shane leon";
+let a;
+let b = "";
+let age = 28;
+let address = null;
+console.log("My name is", name);
+console.log("My age is", age);
+console.log(a);
+console.log(b);
+console.log(address);
+
+ // blank space due to ""
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof true); 
+console.log(typeof address);
+let isAge=25;
+if (isAge==24) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+console.log(typeof(isAge));
+
+// Mathematical Operations
+
+let num1 = 20;
+let num2 = 10;
+
+console.log('The added value is',num1+num2);
+console.log('The subtracted value is',num1-num2);
+console.log('The multiplied value is',num1*num2);
+console.log('The divided value is',num1/num2);
+console.log('The reminder is',num1%num2);
+
+// // comparator operators
+
+let number1 = 60;
+let number2 = 70;
+
+console.log(number1 == number2);
+console.log(number1 === number2);
+console.log(number1 <= number2);
+console.log(number1 >= number2);
+console.log(number1 != number2);
+console.log(number1 !== number2);
+console.log(number1 > number2);
+console.log(number1 < number2);
+
+
+let number_1 = 10;
+let number_2 = 10;
+
+console.log(number1 == number2);
+console.log(number1 === number2);
+console.log(number1 <= number2);
+console.log(number1 >= number2);
+console.log(number1 != number2);
+console.log(number1 !== number2);
+console.log(number1 > number2);
+console.log(number1 < number2);
